1d028b4e8ee4619347360f542e22d6d5ebd465c4
Alexander Serebryakov <alex.serebr@gmail.com>
2012-12-02 23:42:24 +0200

    SDL interface finished

--- i_sdl_video.c
+++ i_sdl_video.c
@@ -122,14 +122,43 @@ void I_FinishUpdate (void)
 	SDL_Flip(screen);
 }
 
-void I_StartTic (void)
-{
 
-}
+//
+//	Palette Stuff
+//
+
+static SDL_Color colors[256];
 
 void I_SetPalette (byte* palette)
 {
+    register int	i;
+    register int	c;
+    static boolean	firstcall = true;
 
+//	TODO: add SDL mode check
+
+	{
+	    // initialize the colormap
+	    if (firstcall)
+	    {
+	    	firstcall = false;
+	    }
+
+	    // set the X colormap entries
+	    for (i=0 ; i<256 ; i++)
+	    {
+	    	c = gammatable[usegamma][*palette++];
+	    	colors[i].r = (c<<8) + c;
+	    	c = gammatable[usegamma][*palette++];
+	    	colors[i].g = (c<<8) + c;
+	    	c = gammatable[usegamma][*palette++];
+	    	colors[i].b = (c<<8) + c;
+	    }
+
+	    // store the colors to the current colormap
+	    SDL_SetColors(screen, colors, 0, 256);
+
+	}
 }
 
 void I_StartFrame (void)
@@ -140,4 +169,192 @@ void I_StartFrame (void)
 void I_ReadScreen (byte* scr)
 {
     memcpy (scr, screens[0], SCREENWIDTH*SCREENHEIGHT);
+}
+
+int xlatekey(SDL_Event* event)
+{
+
+    int rc;
+
+    switch(rc = event->key.keysym.sym)
+    {
+      case SDLK_LEFT:	rc = KEY_LEFTARROW;	break;
+      case SDLK_RIGHT:	rc = KEY_RIGHTARROW;	break;
+      case SDLK_DOWN:	rc = KEY_DOWNARROW;	break;
+      case SDLK_UP:	rc = KEY_UPARROW;	break;
+      case SDLK_ESCAPE:	rc = KEY_ESCAPE;	break;
+      case SDLK_RETURN:	rc = KEY_ENTER;		break;
+      case SDLK_TAB:	rc = KEY_TAB;		break;
+      case SDLK_F1:	rc = KEY_F1;		break;
+      case SDLK_F2:	rc = KEY_F2;		break;
+      case SDLK_F3:	rc = KEY_F3;		break;
+      case SDLK_F4:	rc = KEY_F4;		break;
+      case SDLK_F5:	rc = KEY_F5;		break;
+      case SDLK_F6:	rc = KEY_F6;		break;
+      case SDLK_F7:	rc = KEY_F7;		break;
+      case SDLK_F8:	rc = KEY_F8;		break;
+      case SDLK_F9:	rc = KEY_F9;		break;
+      case SDLK_F10:	rc = KEY_F10;		break;
+      case SDLK_F11:	rc = KEY_F11;		break;
+      case SDLK_F12:	rc = KEY_F12;		break;
+
+      case SDLK_BACKSPACE:
+      case SDLK_DELETE:	rc = KEY_BACKSPACE;	break;
+
+      case SDLK_PAUSE:	rc = KEY_PAUSE;		break;
+
+      case SDLK_KP_EQUALS:
+      case SDLK_EQUALS:	rc = KEY_EQUALS;	break;
+
+      case SDLK_KP_MINUS:
+      case SDLK_MINUS:	rc = KEY_MINUS;		break;
+
+      case SDLK_RSHIFT:
+      case SDLK_LSHIFT:
+	rc = KEY_RSHIFT;
+	break;
+
+      case SDLK_LCTRL:
+      case SDLK_RCTRL:
+	rc = KEY_RCTRL;
+	break;
+
+      case SDLK_LALT:
+      case SDLK_LMETA:
+      case SDLK_RALT:
+      case SDLK_RMETA:
+	rc = KEY_RALT;
+	break;
+
+      default:
+	if (rc >= SDLK_SPACE && rc <= SDLK_SLASH)
+	    rc = rc - SDLK_SPACE + ' ';
+	if (rc >= 'A' && rc <= 'Z')
+	    rc = rc - 'A' + 'a';
+	break;
+    }
+
+    return rc;
+
+}
+
+
+void I_ProcessEvent(SDL_Event* sdl_event)
+{
+    event_t event;
+
+    // put event-grabbing stuff in here
+    switch (sdl_event->type)
+    {
+      case SDL_KEYDOWN:
+	event.type = ev_keydown;
+	event.data1 = xlatekey(sdl_event);
+	D_PostEvent(&event);
+	// fprintf(stderr, "k");
+	break;
+      case SDL_KEYUP:
+	event.type = ev_keyup;
+	event.data1 = xlatekey(sdl_event);
+	D_PostEvent(&event);
+	// fprintf(stderr, "ku");
+	break;
+/*      case SDL_MOUSEBUTTONDOWN:
+	event.type = ev_mouse;
+	event.data1 =
+	    (X_event.xbutton.state & Button1Mask)
+	    | (X_event.xbutton.state & Button2Mask ? 2 : 0)
+	    | (X_event.xbutton.state & Button3Mask ? 4 : 0)
+	    | (X_event.xbutton.button == Button1)
+	    | (X_event.xbutton.button == Button2 ? 2 : 0)
+	    | (X_event.xbutton.button == Button3 ? 4 : 0);
+	event.data2 = event.data3 = 0;
+	D_PostEvent(&event);
+	// fprintf(stderr, "b");
+	break;
+      case SDL_MOUSEBUTTONUP:
+	event.type = ev_mouse;
+	event.data1 =
+	    (X_event.xbutton.state & Button1Mask)
+	    | (X_event.xbutton.state & Button2Mask ? 2 : 0)
+	    | (X_event.xbutton.state & Button3Mask ? 4 : 0);
+	// suggest parentheses around arithmetic in operand of |
+	event.data1 =
+	    event.data1
+	    ^ (X_event.xbutton.button == Button1 ? 1 : 0)
+	    ^ (X_event.xbutton.button == Button2 ? 2 : 0)
+	    ^ (X_event.xbutton.button == Button3 ? 4 : 0);
+	event.data2 = event.data3 = 0;
+	D_PostEvent(&event);
+	// fprintf(stderr, "bu");
+	break;
+      case MotionNotify:
+	event.type = ev_mouse;
+	event.data1 =
+	    (X_event.xmotion.state & Button1Mask)
+	    | (X_event.xmotion.state & Button2Mask ? 2 : 0)
+	    | (X_event.xmotion.state & Button3Mask ? 4 : 0);
+	event.data2 = (X_event.xmotion.x - lastmousex) << 2;
+	event.data3 = (lastmousey - X_event.xmotion.y) << 2;
+
+	if (event.data2 || event.data3)
+	{
+	    lastmousex = X_event.xmotion.x;
+	    lastmousey = X_event.xmotion.y;
+	    if (X_event.xmotion.x != X_width/2 &&
+		X_event.xmotion.y != X_height/2)
+	    {
+		D_PostEvent(&event);
+		// fprintf(stderr, "m");
+		mousemoved = false;
+	    } else
+	    {
+		mousemoved = true;
+	    }
+	}
+	break;
+
+      case Expose:
+      case ConfigureNotify:
+	break;
+*/
+      default:
+//	if (doShm && X_event.type == X_shmeventtype) shmFinished = true;
+	break;
+    }
+
+}
+
+
+void I_StartTic (void)
+{
+
+    if (!screen)
+	return;
+
+    SDL_Event sdl_event;
+
+    while (SDL_PollEvent(&sdl_event))
+    	I_ProcessEvent(&sdl_event);
+
+    // Warp the pointer back to the middle of the window
+    //  or it will wander off - that is, the game will
+    //  loose input focus within X11.
+
+/*    if (grabMouse)
+    {
+	if (!--doPointerWarp)
+	{
+	    XWarpPointer( X_display,
+			  None,
+			  X_mainWindow,
+			  0, 0,
+			  0, 0,
+			  X_width/2, X_height/2);
+
+	    doPointerWarp = POINTER_WARP_COUNTDOWN;
+	}
+    }
+
+    mousemoved = false; */
+
 }
